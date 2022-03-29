6c759fd47c4aa0e230b467d628ab147f88f51397
Ravi Sadariya <ravisadariya03@gmail.com>
2022-03-29 10:27:04 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Navbar from "./Components/Pages/Navbar";
 import AddUser from "./Components/User/AddUser";
 import EditUser from "./Components/User/EditUser";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import NotFound from "./Components/Pages/NotFound";
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
           <Route  path="/" element={<Home />} />
           <Route  path="/users/adduser" element={<AddUser />} />
           <Route  path="/users/edituser/:id" element={<EditUser />} />
-          <Route element={<NotFound />} />
         </Routes>
       </Router>
     </>
